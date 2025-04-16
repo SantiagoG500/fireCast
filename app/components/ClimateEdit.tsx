8a3a5397fa127f933174d50851e61714c59ab543
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-04-16 16:51:53 -0500

    Data interactivity added in Simulator page

--- app/components/ClimateEdit.tsx
+++ app/components/ClimateEdit.tsx
@@ -1,63 +1,100 @@
-// import { getKeyValue,
-//  Input, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from '@heroui/react'
-import { Form, NumberInput } from '@heroui/react'
+import { NumberInput } from '@heroui/react'
 import { ClimateData } from '~/types'
 
 type ClimateEditTableProps = {
-  data: ClimateData | undefined,
+  data: ClimateData,
+  handleClimateDataChange:  (data: ClimateData) => void
 }
 
-export function ClimateEdit({ data }: ClimateEditTableProps) {
+export function ClimateEdit({ data, handleClimateDataChange }: ClimateEditTableProps) {
   return (
     <>
       {
         data
         ? (
-            <Form>
+            <>
               <div className='flex justify-center flex-wrap gap-2'>
                 <NumberInput
                   className='w-5/12'
                   label='k'
+                  name='tasa_de_cambio_k'
+
+                  placeholder={`${data.tasa_de_cambio_k}`}
                   value={data.tasa_de_cambio_k}
+                  onValueChange={(val) => handleClimateDataChange({...data, tasa_de_cambio_k: val})}
                 />
                 <NumberInput
                   className='w-5/12'
                   label='CT'
+                  name='CT'
+
+                  placeholder={`${data.CT}`}
                   value={data.CT}
-                  />
+                  onValueChange={(val) => handleClimateDataChange({...data, CT: val})} 
+                />
                 <NumberInput
                   className='w-5/12'
                   label='CH'
+                  name='CH'
+
+                  placeholder={`${data.CH}`}
                   value={data.CH}
-                  />
+                  onValueChange={(val) => handleClimateDataChange({...data, CH: val})}
+                />
                 <NumberInput
                   className='w-5/12'
                   label='CV'
+                  name='CV'
+
+                  placeholder={`${data.CV}`}
                   value={data.CV}
-                  />
+                  onValueChange={(val) => handleClimateDataChange({...data, CV: val})}
+                />
                 <NumberInput
                   className='w-5/12'
                   label='CS'
+                  name='CS'
+
+                  placeholder={`${data.CS}`}
                   value={data.CS}
-                  />
+                  onValueChange={(val) => handleClimateDataChange({...data, CS: val})}
+                />
 
                 <NumberInput
                   className='w-5/12'
                   label='temp y sequedad' 
+                  name='interaccion_temp_sequedad'
+
+                  placeholder={`${data.interaccion_temp_sequedad}`}
                   value={data.interaccion_temp_sequedad}
-                />
+                  onValueChange={(val) => handleClimateDataChange({...data, interaccion_temp_sequedad: val})}
+              />
                 <NumberInput
                   className='w-5/12'
-                  label='coeficiente total' value={data.coeficiente_total}
+                  name='coeficiente_total'
+                  label='coeficiente total'
+
+                  placeholder={`${data.coeficiente_total}`}
+                  value={data.coeficiente_total}
+                  onValueChange={(val) => handleClimateDataChange({...data, coeficiente_total: val})}
                 />
 
                 <NumberInput
                   className='w-5/12'
                   label='funcion logistica' 
+                  name='funcion_logistica_aplicada'
+
+                  placeholder={`${data.funcion_logistica_aplicada}`}
                   value={data.funcion_logistica_aplicada}
+                  onValueChange={(val) => handleClimateDataChange({...data, funcion_logistica_aplicada: val})}
                 />
               </div>
-            </Form>
+
+              {/* <div className='flex justify-center gap-4 w-full mt-4'>
+                <Button variant='flat' color='default'>Reset</Button>
+                <Button variant='flat' color='primary'>Apply</Button>
+              </div> */}
+            </>
           )
         : 'No Data...'
       }
--- app/hooks/useClimateData.ts
+++ app/hooks/useClimateData.ts
@@ -1,22 +1,67 @@
 import { useEffect, useState } from 'react';
-import { ClimateData } from '../types/index'; 
+import { ClimateData, ColumnClimateDataTable } from '../types/index'; 
+import initialClimateState from "../../mocks/datos-climaticos.json";
 
-
-export function useClimateData({ data }: {data: ClimateData[]}) {
+/**
+ * **Manages Climatic data**
+ * 
+ *  Returns:
+ * - monthData: State of the selected month **(only the name in string).**
+ * - climateData: State of the Climate data (in array) {@link ClimateData}.
+ * - columns: Array with the keys of the data type {@link ClimateData}
+ * 
+ * - setCurrentMonth
+ * - handleMonthData
+ * - resetClimateData
+ * - handleClimateDataChange
+ * 
+ */
+export function useClimateData() {
   const [month, setMonth] = useState("Enero");
-  const [monthData, setMonthData] = useState<ClimateData | undefined>(undefined)
+  const [monthData, setMonthData] = useState<ClimateData>(initialClimateState[0])
 
-  const handleMonth = (month: string) => {
-    setMonth(month)
-  }
-
-  useEffect(() => {
-    const currentMonth = data.find(row => row.mes === month)  
-    setMonthData(currentMonth)  
-  }, [month, data])
+  const [climateData, setClimateData] = useState<ClimateData[]>(initialClimateState)
   
+  const columns: ColumnClimateDataTable[] = Object.keys(initialClimateState[0]).map((key) => {
+    return{
+      key,
+      label:key
+    }
+  })
+  
+  const handleClimateDataChange = (monthData: ClimateData) => {
+    setClimateData((prev) => prev.map((val) =>
+        val.mes === monthData.mes 
+          ? monthData
+          : val
+      )
+    )  
+  }
+  const handleMonthData = (data: ClimateData) => {
+    setMonthData(data)
+  }
+  
+  const resetClimateData = () => {
+    setClimateData(initialClimateState)
+    setMonthData(initialClimateState[0])
+  }
+  
+  useEffect(() => {
+    const newMonth = climateData.find(row => row.mes === month)  
+    
+    setMonthData(newMonth ?? initialClimateState[0])  
+  }, [month, climateData])
+  
+  const setCurrentMonth = (month: string) => { setMonth(month) }
+
   return {
     monthData,
-    handleMonth
+    climateData,
+    columns,
+    
+    setCurrentMonth,
+    handleMonthData,
+    resetClimateData,
+    handleClimateDataChange,
   }
 }
